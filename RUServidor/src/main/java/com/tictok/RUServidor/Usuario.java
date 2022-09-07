9742cc13b382e00626ea2ee1b7d5c444dc071468
unknown <agustinpanizza123@gmail.com>
2022-09-07 15:36:02 -0300

    Agregué el usuarioEntity y el UsuarioRepository al Server

---
+++ RUServidor/src/main/java/com/tictok/RUServidor/Usuario.java
@@ -0,0 +1,53 @@
+package com.tictok.RUServidor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Usuario {
+
+
+    @Column (name= "nombre")
+    private String nombre;
+    @Column
+    private String direc;
+    @Id
+    private String telefono;
+
+    public Usuario(String nombre, String direc, String telefono) {
+        this.nombre = nombre;
+        this.direc = direc;
+        this.telefono = telefono;
+    }
+
+    public Usuario() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDirec() {
+        return direc;
+    }
+
+    public void setDirec(String direc) {
+        this.direc = direc;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
