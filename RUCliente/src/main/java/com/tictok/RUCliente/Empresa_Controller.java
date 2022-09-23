a7bfe3f16f41dac9d00372b59a304ebefeaa3141
mvdelcampo <105162990+mvdelcampo@users.noreply.github.com>
2022-09-23 15:14:10 -0300

    cambie ventanitas

--- RUCliente/src/main/java/com/tictok/RUCliente/Empresa_Controller.java
+++ RUCliente/src/main/java/com/tictok/RUCliente/Empresa_Controller.java
@@ -6,14 +6,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component //importante!!!!!
-public class Controller_Vista1_JavaFX {
+public class Empresa_Controller {
     public Button btnSalir;
     public Button btnRegistrarNC;
 
@@ -24,12 +23,12 @@ public class Controller_Vista1_JavaFX {
 
     }
 
-    public void pasarAVista2(ActionEvent actionEvent) throws IOException {
+    public void registrarUsuario(ActionEvent actionEvent) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setControllerFactory(Main.getContext()::getBean);
 
-        Parent root = fxmlLoader.load(Controller_Vista2_JavaFX.class.getResourceAsStream("vista2_archivo.fxml"));
+        Parent root = fxmlLoader.load(Empresa_RegistroEmpl_Controller.class.getResourceAsStream("empresa_registro_empleado.fxml"));
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Registrar nuevo empleado");
