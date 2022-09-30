7152f44a2112d4ea7c4a7618adfc6ea039230d3f
mariagracianin <mariagracia.nin@gmail.com>
2022-09-30 19:56:22 -0300

    mas mappers

--- RUServidor/src/main/java/com/tictok/RUServidor/Mappers/CuentaMapper.java
+++ RUServidor/src/main/java/com/tictok/RUServidor/Mappers/CuentaMapper.java
@@ -1,11 +1,13 @@
 package com.tictok.RUServidor.Mappers;
 
 import com.tictok.Commons.CuentaDTO;
+import com.tictok.Commons.MegaUsuarioDTO;
 import com.tictok.Commons.UsuarioDTO;
 import com.tictok.RUServidor.Entities.Cuenta;
+import com.tictok.RUServidor.Entities.Usuario;
 
 public class CuentaMapper {
-    public static Object toCuentaDTO(Cuenta cuenta) {
+    public static CuentaDTO toCuentaDTO(Cuenta cuenta) {
 
         return new CuentaDTO(cuenta.getId(), cuenta.getPassword(),
                 cuenta.getTipo());
@@ -14,5 +16,12 @@ public class CuentaMapper {
 
     public static Cuenta toCuenta(CuentaDTO cuentaDTO) {
         return new Cuenta(cuentaDTO.getMail(), cuentaDTO.getPassword(), cuentaDTO.getTipo());
+    }
+
+    public static Cuenta toCuentaFromMegaUsuarioDTO(MegaUsuarioDTO megauserDTO){
+        if(megauserDTO == null){
+            return null;
+        }
+        return new Cuenta(megauserDTO.getCuentaMail(),megauserDTO.getPassword(),"user");
     }
 }
--- RUServidor/src/main/java/com/tictok/RUServidor/Mappers/UsuarioMapper.java
+++ RUServidor/src/main/java/com/tictok/RUServidor/Mappers/UsuarioMapper.java
@@ -1,27 +1,39 @@
 package com.tictok.RUServidor.Mappers;
 
+import com.tictok.Commons.MegaUsuarioDTO;
 import com.tictok.RUServidor.Entities.Usuario;
 import com.tictok.Commons.UsuarioDTO;
 
 public class UsuarioMapper {
     public static Usuario toUsuario(UsuarioDTO userDTO){
 
-//        if(userDTO != null){
-//            return null;
-//        }
+        if(userDTO == null){
+            return null;
+        }
         return new Usuario(userDTO.getCedula(), userDTO.getVencimientoCarne(),userDTO.getNombre(),
                 userDTO.getApellido(), userDTO.getTelefono(),userDTO.getSaldoBase(),userDTO.getSobregiro(),userDTO.getSaldo());
     }
 
     public static UsuarioDTO toUsuarioDTO(Usuario user){
 
-//        if(user != null){
-//            return null;
-//        }
+        if(user == null){
+            return null;
+        }
 
         return new UsuarioDTO(user.getStringCuenta(), user.getCedula(),user.getVencimientoCarne(),user.getNombre(),user.getApellido(),user.getTelefono()
                 ,user.getSaldoBase(),user.getSobregiro(),user.getSaldo());
     }
+
+    public static Usuario toUsuarioFromMegaUsuarioDTO(MegaUsuarioDTO megauserDTO){
+        if(megauserDTO == null){
+            return null;
+        }
+        return new Usuario(megauserDTO.getCedula(), megauserDTO.getVencimientoCarne(),megauserDTO.getNombre(),
+                megauserDTO.getApellido(), megauserDTO.getTelefono(),megauserDTO.getSaldoBase(),megauserDTO.getSobregiro(),megauserDTO.getSaldo());
+
+    }
+
+    //
 
     public static Usuario toUpdateUsuario(Usuario user, Usuario updateUser){
         //?
