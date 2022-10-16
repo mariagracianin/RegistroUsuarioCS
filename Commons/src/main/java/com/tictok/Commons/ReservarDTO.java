63bb6aa1ae441f73f1fe9b9faa8918c5301ba0c2
Agustín Panizza <agustinpanizza123@gmail.com>
2022-10-16 15:48:42 -0300

    Creada la ReservaDTO

--- Commons/src/main/java/com/tictok/Commons/ReservarDTO.java
+++ Commons/src/main/java/com/tictok/Commons/ReservarDTO.java
@@ -4,50 +4,86 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReservarDTO {
-//    private String mailUsuario;
-//    private String nombreCentro;
-//    private String nombreActividad;
-//    private HorarioDTO horario;
-//
-//    public ReservarDTO() {
-//    }
-//
-//    public ReservarDTO(String mailUsuario, String nombreCentro, String nombreActividad, HorarioDTO horario) {
-//        this.mailUsuario = mailUsuario;
-//        this.nombreCentro = nombreCentro;
-//        this.nombreActividad = nombreActividad;
-//        this.horario = horario;
-//    }
-//
-//    public String getMailUsuario() {
-//        return mailUsuario;
-//    }
-//
-//    public void setMailUsuario(String mailUsuario) {
-//        this.mailUsuario = mailUsuario;
-//    }
-//
-//    public String getNombreCentro() {
-//        return nombreCentro;
-//    }
-//
-//    public void setNombreCentro(String nombreCentro) {
-//        this.nombreCentro = nombreCentro;
-//    }
-//
-//    public String getNombreActividad() {
-//        return nombreActividad;
-//    }
-//
-//    public void setNombreActividad(String nombreActividad) {
-//        this.nombreActividad = nombreActividad;
-//    }
-//
-//    public HorarioDTO getHorario() {
-//        return horario;
-//    }
-//
-//    public void setHorario(HorarioDTO horario) {
-//        this.horario = horario;
-//    }
+    private String mailUsuario;
+    private String nombreCentro;
+    private String nombreActividad;
+    private HorarioDTO horario;
+
+    private Long codigoReserva;
+    private Long codigoReservaPadre; //Usado en las canchas para pedir el codigo de la reserva del que es padre
+
+    public ReservarDTO() {
+    }
+
+    public ReservarDTO(String mailUsuario, String nombreCentro, String nombreActividad, HorarioDTO horario) {
+        this.mailUsuario = mailUsuario;
+        this.nombreCentro = nombreCentro;
+        this.nombreActividad = nombreActividad;
+        this.horario = horario;
+    }
+
+    public ReservarDTO(String mailUsuario, String nombreCentro, String nombreActividad, HorarioDTO horario, Long codigoReserva) {
+        this.mailUsuario = mailUsuario;
+        this.nombreCentro = nombreCentro;
+        this.nombreActividad = nombreActividad;
+        this.horario = horario;
+        this.codigoReserva = codigoReserva;
+    }
+
+    public ReservarDTO(String mailUsuario, String nombreCentro, String nombreActividad, HorarioDTO horario, Long codigoReserva, Long codigoReservaPadre) {
+        this.mailUsuario = mailUsuario;
+        this.nombreCentro = nombreCentro;
+        this.nombreActividad = nombreActividad;
+        this.horario = horario;
+        this.codigoReserva = codigoReserva;
+        this.codigoReservaPadre = codigoReservaPadre;
+    }
+
+    public String getMailUsuario() {
+        return mailUsuario;
+    }
+
+    public void setMailUsuario(String mailUsuario) {
+        this.mailUsuario = mailUsuario;
+    }
+
+    public String getNombreCentro() {
+        return nombreCentro;
+    }
+
+    public void setNombreCentro(String nombreCentro) {
+        this.nombreCentro = nombreCentro;
+    }
+
+    public String getNombreActividad() {
+        return nombreActividad;
+    }
+
+    public void setNombreActividad(String nombreActividad) {
+        this.nombreActividad = nombreActividad;
+    }
+
+    public HorarioDTO getHorario() {
+        return horario;
+    }
+
+    public void setHorario(HorarioDTO horario) {
+        this.horario = horario;
+    }
+
+    public Long getCodigoReserva() {
+        return codigoReserva;
+    }
+
+    public void setCodigoReserva(Long codigoReserva) {
+        this.codigoReserva = codigoReserva;
+    }
+
+    public Long getCodigoReservaPadre() {
+        return codigoReservaPadre;
+    }
+
+    public void setCodigoReservaPadre(Long codigoReservaPadre) {
+        this.codigoReservaPadre = codigoReservaPadre;
+    }
 }
--- RUCliente/src/main/java/com/tictok/RUCliente/UsuarioRest.java
+++ RUCliente/src/main/java/com/tictok/RUCliente/UsuarioRest.java
@@ -63,26 +63,26 @@ public class UsuarioRest {
     }
 
     //NO FUNCIONA SEGURO :(
-    public HttpResponse<String> hacerReserva(String mailUsuario, String nombreCentro, String nombreActividad, HorarioDTO horario){
-        String reservarJSON = "";
-        try {
-            ObjectMapper jsonObjectMapper = new ObjectMapper();
-            ReservarDTO reservarDTO = new ReservarDTO(mailUsuario,nombreCentro,nombreActividad,horario);
-            reservarJSON = jsonObjectMapper.writeValueAsString(reservarDTO);
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-
-        try {
-            HttpResponse<String> response = Unirest.post("http://localhost:8080/NO SE AUN")
-                    .header("Content-Type", "application/json")
-                    .body(reservarJSON)
-                    .asString();
-            return response;
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-
-    }
+//    public HttpResponse<String> hacerReserva(String mailUsuario, String nombreCentro, String nombreActividad, HorarioDTO horario){
+//        String reservarJSON = "";
+//        try {
+//            ObjectMapper jsonObjectMapper = new ObjectMapper();
+//            ReservarDTO reservarDTO = new ReservarDTO(mailUsuario,nombreCentro,nombreActividad,horario);
+//            reservarJSON = jsonObjectMapper.writeValueAsString(reservarDTO);
+//        }catch (Exception e){
+//            throw new RuntimeException(e);
+//        }
+//
+//        try {
+//            HttpResponse<String> response = Unirest.post("http://localhost:8080/NO SE AUN")
+//                    .header("Content-Type", "application/json")
+//                    .body(reservarJSON)
+//                    .asString();
+//            return response;
+//        }catch (Exception e){
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
 
 }
