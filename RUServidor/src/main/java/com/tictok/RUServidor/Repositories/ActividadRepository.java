9a0939d406bb3a4f6131209c95c7446e2f198afe
mariagracianin <mariagracia.nin@gmail.com>
2022-10-21 18:42:01 -0300

    filtro actividad

--- RUServidor/src/main/java/com/tictok/RUServidor/Repositories/ActividadRepository.java
+++ RUServidor/src/main/java/com/tictok/RUServidor/Repositories/ActividadRepository.java
@@ -14,5 +14,5 @@ public interface ActividadRepository extends JpaRepository<Actividad, ServicioId
     @Query("""
             select a from Actividad a
             where upper(a.actividadId.nombreServicio) like upper(?1) or upper(a.actividadId.centroDeportivo) like upper(?2) or upper(a.centroDeportivo.barrio) like upper(?3) or upper(a.centroDeportivo.address) like upper(?4)""")
-    List<Cancha> findByNombreOBarrioIsLike(String nombreServicio, String centroDeportivo, String barrio, String address);
+    List<Actividad> findByNombreOBarrioIsLike(String nombreServicio, String centroDeportivo, String barrio, String address);
 }
