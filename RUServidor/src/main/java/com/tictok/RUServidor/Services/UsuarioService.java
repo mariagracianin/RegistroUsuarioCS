05dd47678f376eda42f93ace7a0d4cf2080479e0
mariagracianin <mariagracia.nin@gmail.com>
2022-11-20 17:25:18 -0300

    ke?

--- RUServidor/src/main/java/com/tictok/RUServidor/Services/UsuarioService.java
+++ RUServidor/src/main/java/com/tictok/RUServidor/Services/UsuarioService.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
